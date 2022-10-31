7e3d0153299e197dba1d72f6c6ae9a47f873a526
ndaboom <ndabosam084@gmail.com>
2022-11-01 00:12:05 +0100

    Remove active class module added

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import screens from "./modules/screens.js";
 import book from "./modules/books.js";
 import currentTime from "./modules/timer.js";
+import removeActive from "./modules/remove-active.js";
 
 const projectInit = () => {
 // Navigation between views
 const optionsMenu = document.querySelectorAll(".menu-option");
-
-function removeActive() {
-  optionsMenu.forEach((option) => {
-    option.classList.remove("blue");
-  });
-}
-
-// Add click listener to menu options
 optionsMenu.forEach((option, index) => {
   option.addEventListener("click", () => {
     removeActive();
